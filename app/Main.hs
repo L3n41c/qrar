04ee4b69aa7e66a2a27caa9266f6dad87deb248a
Lénaïc Huard <lenaic@lhuard.fr>
2016-11-20 22:38:15 +0100

    Very first implementation

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,43 @@
 module Main where
 
 import Prelude.Unicode
+import Control.Monad.Unicode
+
+import Control.Monad (filterM, liftM, forM_)
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Text.IO (readFile)
+import System.Directory (doesFileExist, renameFile)
+import System.Exit (ExitCode(..))
+import System.File.Tree (getDirectory, flatten)
+import System.FilePath ((</>))
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (createProcess, waitForProcess, proc, cwd, std_out, delegate_ctlc, StdStream(CreatePipe))
+import Text.LaTeX.Base.Render (renderFile)
+
 import Lib
 
 main ∷ IO ()
-main = someFunc
+main =
+  withSystemTempDirectory "qrar" $ \tmpDir → do
+    generateTexFile "." (tmpDir </> "qrar.tex")
+    (_, Just hout, _, ph) ← createProcess (proc "pdflatex" ["qrar.tex"]){ cwd = Just tmpDir
+                                                                        , std_out = CreatePipe
+                                                                        , delegate_ctlc = True }
+    out ← B.hGetContents hout
+    ec ← waitForProcess ph
+    case ec of
+      ExitFailure r → do
+        B.putStrLn out
+        forM_ ["qrar.tex", "qrar.log"] $ \f →
+          renameFile (tmpDir </> f) f
+        ioError ∘ userError $ "pdflatex"
+      ExitSuccess → renameFile (tmpDir </> "qrar.pdf") "qrar.pdf"
+
+generateTexFile ∷ FilePath → FilePath → IO ()
+generateTexFile rootDir texFileName =
+      return rootDir
+  ≫= getDirectory
+  ≫= return ∘ flatten
+  ≫= filterM doesFileExist
+  ≫= mapM (\x → liftM ((,) x) (Data.Text.IO.readFile x))
+  ≫= renderFile texFileName ∘ filelistToLatex
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,36 @@
 {- -*- mode: haskell; eval: (turn-on-haskell-unicode-input-method) -*- -}
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE UnicodeSyntax, OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( filelistToLatex
     ) where
 
 import Prelude.Unicode
+import Data.Monoid.Unicode
 
-someFunc ∷ IO ()
-someFunc = putStrLn "someFunc"
+import Text.LaTeX
+import Text.LaTeX.Packages.Fontenc
+import Text.LaTeX.Packages.Inputenc
+import Text.LaTeX.Packages.QRCode
+
+filelistToLatex ∷ [(FilePath, Text)] → LaTeX
+filelistToLatex filelist =
+  thePreamble ⊕ document (theBody filelist)
+
+thePreamble ∷ LaTeX
+thePreamble =
+    documentclass [Paper A4] article
+  ⊕ useencoding [T1]
+  ⊕ usepackage [utf8] inputenc
+  ⊕ usepackage [] qrcode
+
+theBody ∷ [(FilePath, Text)] → LaTeX
+theBody filelist =
+  mconcat $ map fileToLatex filelist
+
+fileToLatex ∷ (FilePath, Text) → LaTeX
+fileToLatex (filepath, filecontent) =
+    section (texttt $ fromString filepath)
+  ⊕ qr CodeOptions{ includePadding = True
+                  , link = False
+                  , errorLevel = High} filecontent
